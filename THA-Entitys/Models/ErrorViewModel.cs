f73b361f574219d685f075f44bf4e6308acc816a
Muradawad20 <68800448+MuradAL-Tarifi@users.noreply.github.com>
2022-02-15 11:00:42 +0200

    Add project files.

---
+++ THA-Entitys/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace THA_Entitys.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
